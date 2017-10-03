d281f40044dc1cc85a1960f57069e51317a0283a
ilayami <31542602+ilayami@users.noreply.github.com>
2017-10-02 20:55:32 -0500

    lab3-listperformance
    This java program will help to find the time measurmertn diferences between linked list and arraylist.

---
+++ cs271/lab/list/src/cs271/lab/list/TestIterator.java
@@ -0,0 +1,117 @@
+package cs271.lab.list;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestIterator {
+
+    //private List<Integer> list;
+    private LinkedList<Integer>list;
+    // See the Java List Interface documentation to understand what all the List methods do ...
+    //Postional Access, Search, Iteration, and Range view(Sublet Method)
+
+    @Before
+    public void setUp() throws Exception {
+        //list = new ArrayList<Integer>();
+        list=new LinkedList<Integer>();
+        // TODO also try with a LinkedList done - does it make any difference?(not sure List is faster than a LinkedList in
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        list = null;
+    }
+
+    @Test
+    public void testEmpty() {
+        final Iterator<Integer> i = list.iterator();
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    public void testNonempty() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        final Iterator<Integer> i = list.iterator();
+        assertTrue(i.hasNext());
+        assertEquals(33, i.next().intValue());
+        // TODO fix the expected values in the assertions below done
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(44, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(55, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(77, i.next().intValue());
+        assertTrue(i.hasNext());
+        assertEquals(66, i.next().intValue());
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    public void testRemove() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        final Iterator<Integer> i = list.iterator();
+        while (i.hasNext()) {
+            if (i.next() == 77) {
+              //list.remove(77);
+                 i.remove();
+                 // (done) TODO what happens if you use list.remove(77)? Failed test called (IndexOutOfBoundsException)
+                // because the size of the list is 7. There is no index 77
+            }
+        }
+        // TODO using assertEquals and Arrays.asList, express which values are left in the list done
+        // See TestList.java for examples of how to use Arrays.asList; also see the Java Arrays
+        //Arrays.asList(33,44,55,66);
+        // class for more information
+      assertEquals(Arrays.asList(33,44,55,66),list);
+        //fail("Not yet implemented"); // remove this line when done
+    }
+
+    @Test
+    public void testAverageValues() {
+        list.add(33);
+        list.add(77);
+        list.add(44);
+        list.add(77);
+        list.add(55);
+        list.add(77);
+        list.add(66);
+        double sum = 0;
+        int n = 0;
+        // TODO use an iterator and a while loop to compute the average (mean) of the values(done)
+        final Iterator<Integer> i = list.iterator();
+        while (i.hasNext() && n <list.size()){
+            sum = sum + list.get(n);
+            n++;
+        }
+        // (defined as the sum of the items divided by the number of items)
+        // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+        //assertEquals(61.3, sum / n, 0.1);
+        assertEquals(61.3, sum / n, 0.1);
+        assertEquals(7, n);
+    }
+}
